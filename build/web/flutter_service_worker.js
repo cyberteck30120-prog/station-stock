dd7d0e38098ea3fea383cc820d6ce0088ac39e77
Kevin <kevin@example.com>
2026-05-05 01:54:55 +0200

    Use real system logo for PWA icons

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"apple-touch-icon.png": "fcf2038bfd4d2314552ab1b2212ecc60",
+const RESOURCES = {"apple-touch-icon.png": "811193df13a5d29995c5003d5304f1ff",
 "assets/AssetManifest.bin": "71073c5be19fa7a451dfbb4d15de6e4f",
 "assets/AssetManifest.bin.json": "c4f55e692d3fceca1901aa13001bfc2e",
 "assets/assets/images/logo_systeme.png": "811193df13a5d29995c5003d5304f1ff",
@@ -29,9 +29,9 @@ const RESOURCES = {"apple-touch-icon.png": "fcf2038bfd4d2314552ab1b2212ecc60",
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "18060240aec6856f291d69facb3a1730",
-"icons/Icon-192.png": "fcf2038bfd4d2314552ab1b2212ecc60",
-"icons/Icon-512.png": "fcf2038bfd4d2314552ab1b2212ecc60",
+"flutter_bootstrap.js": "05c2d5b6ef12a4afe236a832331e0ead",
+"icons/Icon-192.png": "811193df13a5d29995c5003d5304f1ff",
+"icons/Icon-512.png": "811193df13a5d29995c5003d5304f1ff",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "2fbdd56466b5db848b79325466a918c4",
